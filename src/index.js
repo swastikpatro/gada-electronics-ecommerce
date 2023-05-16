bdf2b9d25b01481606a05984d6575e3a4c2bab47
swastikpatro <patroswastik.jsdev@gmail.com>
2023-05-16 19:25:28 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import { makeServer } from "./server";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import { makeServer } from './server';
 
 // Call make Server
 makeServer();
 
-ReactDOM.render(
+const root = createRoot(document.getElementById('root'));
+
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
