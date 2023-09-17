4144d180c455d96bd592be123e702720acfa8c4b
Ayush Agrawal <87698717+Pegasus321@users.noreply.github.com>
2023-09-17 21:56:30 +0530

    Update index.js
    updated the  import path for "normalize.css" to "normalize.css/normalize.css'

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import 'normalize.css';
+import 'normalize.css/normalize.css';
 import './index.css';
 import { makeServer } from './server';
 import {
